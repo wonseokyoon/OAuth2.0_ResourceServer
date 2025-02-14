0d38f0433be093a1b875e66579450eb5904cd453
wonseokyoon <01088115030a@gmail.com>
2025-02-14 17:01:28 +0900

    first commit

---
+++ src/main/java/springsecurity/Oauth2_ResourceServer.java
@@ -0,0 +1,13 @@
+package springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2_ResourceServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2_ResourceServer.class, args);
+    }
+
+}
